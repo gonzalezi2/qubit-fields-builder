69bef7402b4a509ab63606243e6d2d9c90423631
Ivan Gonzalez <ivangonzalez@mindera.com>
2019-09-19 13:13:34 -0700

    test(QFB-27, QFB-13): add tests for the preview-pane component

---
+++ src/components/preview-pane/previewPane.test.js
@@ -0,0 +1,32 @@
+import { h } from 'preact';
+import FieldPreview from './components/field-preview';
+import PreviewPane from '.';
+import { shallow } from 'enzyme';
+
+describe('Preview Pane', () => {
+	it('should render with the right elements and without any errors', () => {
+		const props = {
+			groups: {}
+		};
+		const component = shallow(<PreviewPane {...props} />);
+		const header = component.find('h3');
+		const svgCloseIcon = component.find('svg.closeIcon');
+
+		expect(component.length).toBe(1);
+		expect(svgCloseIcon.length).toBe(1);
+		expect(header.text()).toBe('Edit experience content');
+	});
+
+	it('should render 2 field-preview components when there are two groups', () => {
+		const props = {
+			groups: {
+				df64d5f4d: {},
+				df1d181dc: {}
+			}
+		};
+		const component = shallow(<PreviewPane {...props} />);
+		const fieldPreview = component.find(FieldPreview);
+
+		expect(fieldPreview.length).toBe(2);
+	});
+});
